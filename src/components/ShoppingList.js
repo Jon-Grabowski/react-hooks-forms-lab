0c7920b0bffa1475a7fa9feab773c26fb3cc28a6
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-06-19 18:33:44 -0400

    Search filter working

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,41 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchValue, setSearchValue] = useState('')
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  function handleSearchChange(e) {
+    e.preventDefault();
+    console.log(e.target.value)
+    setSearchValue(e.target.value)
+  }
+
+  const filteredItems = items.filter((item) => {
     if (selectedCategory === "All") return true;
 
     return item.category === selectedCategory;
   });
 
+  const itemsToDisplay = filteredItems.filter(item => {
+    const casedItem = item.name.toLowerCase()
+    if (searchValue === "") return true;
+
+    return casedItem.includes(searchValue.toLowerCase());
+  })
+    
+  
+
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter 
+        onCategoryChange={handleCategoryChange}
+        onSearchChange={handleSearchChange}
+        search={searchValue} 
+        />
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
